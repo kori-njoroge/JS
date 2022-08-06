55fe2f05e34cc58a4d8da936318239c2229e20cf
Kori-njoroge <korinjoroge63@gmai.com>
2022-08-07 01:01:20 +0300

    For Ninjas

--- FunctionalProgramming/MapOnPrototype.js
+++ FunctionalProgramming/MapOnPrototype.js
@@ -1,5 +1,14 @@
 /**
- *
+ *Implement map on a Prototype
+
+As you have seen from applying Array.prototype.map(), or simply map() earlier, the map method returns an array of the same length as the one it was called on. It also doesn't alter the original array, as long as its callback function doesn't.
+
+In other words, map is a pure function, and its output depends solely on its inputs. Plus, it takes another function as its argument.
+
+You might learn a lot about the map method if you implement your own version of it. It is recommended you use a for loop or Array.prototype.forEach().
+
+Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map(). You should not use the built-in map method. The Array instance can be accessed in the myMap method using this.
+
  */
 // The global variable
 const s = [23, 65, 98, 5];
@@ -7,11 +16,11 @@ const s = [23, 65, 98, 5];
 Array.prototype.myMap = function (callback) {
     const newArray = [];
     // Only change code below this line
-    for (let j = 0; j < callback.length; j++) {
+    for (let j = 0; j < this.length; j++) {
         // console.log(j);
-        if (callback[j] > 0) {
-            // console.log(callback[j])
-            newArray.push(callback[j]);
+        if (this[j] > 0) {
+            // console.log(this[j])
+            newArray.push(this[j]);
         }
     }
     // Only change code above this line
@@ -21,4 +30,4 @@ Array.prototype.myMap = function (callback) {
 const new_s = s.myMap(function (item) {
     return item * 2;
 });
-// console.log(new_s);
+console.log(new_s);
